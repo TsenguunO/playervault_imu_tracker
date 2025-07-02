2460afd3fa6232ff732b5dc93fce138b4f08dd60
Tim Tsenguun <tsenguunpy@gmail.com>
2025-07-02 20:28:58 +1000

    last 10 data

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Request
+from collections import deque
 
 app = FastAPI()
-
-received_data = []
+imu_log = deque(maxlen=100)  # store last 100 packets
 
 @app.get("/")
 def health_check():
@@ -12,9 +12,9 @@ def health_check():
 async def receive_imu(request: Request):
     data = await request.json()
     print("📥 Received IMU Data:", data)
-    received_data.append(data)
+    imu_log.append(data)
     return {"status": "received"}
 
 @app.get("/log")
-def get_latest_data():
-    return received_data[-10:]  # Show last 10 packets
+def get_log():
+    return list(imu_log)[-10:]  # return last 10 packets
